75b9a92e51a027c420312ecbecbd65cec14450c6
jojoonhwan <cojh4120@naver.com>
2016-04-24 21:41:59 +0900

    Update assignment1_subscribe.py

--- assignment1_subscribe.py
+++ assignment1_subscribe.py
@@ -1,45 +1,45 @@
 import paho.mqtt.client as mqtt
 
-def on_connect(clinet,userdata, flags, rc):
+def on_connect(clinet,userdata, flags, rc):  # client가 server에서 응답받을 시 callback
 
-	client.subscribe("environment/temperature")
-	client.subscribe("environment/humidity")
+	client.subscribe("environment/temperature") # temperature topic을 subscribe하여 읽음
+	client.subscribe("environment/humidity")  # humidity topic을 subscribe하여 읽음
 
-def on_message(client,userdata, message):
+def on_message(client,userdata, message):  # server에서 message 받을 시 callback
 
-	global temp, humi, temp1, humi1
+	global temp, humi, temp1, humi1 # 전역 변수 선언
 
-	if message.topic == "environment/temperature":
+	if message.topic == "environment/temperature": # topic이 temperature 일 때 출력
 
 		temp = float(message.payload)
 		temp1 = True
 
-	elif message.topic == "environment/humidity":
+	elif message.topic == "environment/humidity": # topic이 humidity 일 때 출력
 
 		humi = float(message.payload)
 		humi1 = True
 
-	if temp1 and humi1:
+	if temp1 and humi1: # temp1 && humi1 연산과 같음
 
-		result = ((9.0/5.0)*temp) - (0.55 *(1.0 - humi) * (((9.0/5.0)*temp) - 26.0)) + 32.0 
+		result = ((9.0/5.0)*temp) - (0.55 *(1.0 - humi) * (((9.0/5.0)*temp) - 26.0)) + 32.0  #불쾌지수
 
 		print "Temperature : " +str(temp)
 		print "Humidity : " +str(humi)
 		print "DIscomport index: " +str(result),
 
-		if result >= 80 :
+		if result >= 80 :  #불쾌지수 80이상이면 very high
 			print "(very high)\n"
-		elif (75<=result) and (result <85):
+		elif (75<=result) and (result <80): # 75 ~ 80일때 high
 			print "(high)\n"
-		elif (65<=result) and (result<75):
+		elif (60<=result) and (result<75): # 60 ~ 75일때 normal
 			print" normal\n"
 		else :
-		 	print"(low)\n"
+		 	print"(low)\n" # 60미만 low
 
 		temp1 = False
 		humi1 = False
 
-
+ # 변수 초기화
 temp = 0
 humi = 0
 
